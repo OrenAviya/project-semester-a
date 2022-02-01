b078bded41d66ded0f19097b87d17aa1c7256da2
Aviya Oren <avoaviya@gmail.com>
2022-02-01 13:39:16 +0200

    initial commit.
    just an example...

---
+++ start _project.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://en.wikipedia.org/wiki/Windows_11')
+soup = BeautifulSoup(res.content, 'html.parser')
+
+labels = soup.select('.infobox.vevent tr .infobox-label')
+datas = soup.select('.infobox.vevent tr .infobox-data')
+for i in range(len(labels)):
+    print(labels[i].getText() + ": " + datas[i].getText())
+
+exit()
+
+children_num = requests.get('https://www.cbs.gov.il/he/mediarelease/pages/2021/%D7%9E%D7%A9%D7%A4%D7%97%D7%95%D7%AA-%D7%91%D7%99%D7%A9%D7%A8%D7%90%D7%9C-%D7%A0%D7%AA%D7%95%D7%A0%D7%99%D7%9D-%D7%9C%D7%A8%D7%92%D7%9C-%D7%99%D7%95%D7%9D-%D7%94%D7%9E%D7%A9%D7%A4%D7%97%D7%94.aspx')
+print (children_num )
+Crime_data = requests.get('https://data.gov.il/dataset/netuny_peshia')
+print(Crime_data.text)
+crimes= BeautifulSoup(Crime_data.text)
+print(crimes, crimes.parser_class)
+ 
+ #from sklearn.pipeline import make_pipeline
