58dea3ecc60f551248b9fb2a7e65f2057aa5d7b8
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2022-02-06 20:55:18 +0200

    " now there is a start of data"

--- crawling_cars_urls.py
+++ crawling_cars_urls.py
@@ -10,31 +10,34 @@ HOST_WEB = 'https://carwiz.co.il'
 driver = sw.Chrome(executable_path="./chromedriver.exe")
 driver.get(HOST_WEB + '/used-cars')
 
+v_columns = ['url','version' , 'year' , 'engine' , 'current_mileage' , 'hand' , 'gearBox' , 'color' , 'original_onership' , 'next_test_in' , 'annual_licensing_fee' , 'price']
+v_indexes = [ 1 , 2, 3, 4, 5 , 6, 7, 8, 9 ,10 ]
+
 car_urls = []
-while len(car_urls) < 1000:
+while (len(car_urls) < 300):
+	
 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 	
 	time.sleep(1)
-
+	
 	try:
 		soup = BeautifulSoup(driver.page_source, 'html.parser')
 	
 		elms = soup.select('#root div main div.MuiContainer-root.MuiContainer-maxWidthLg > div:nth-child(2) > div > div > div a[href]')
 		
 		#elms is a list of all  the links in the page , each link represents page for one car:
-		
+		i = 0
 		for elm in elms:
 			car_url = HOST_WEB + elm['href']
 			if car_url not in car_urls:
 				car_urls.append(car_url)
 				print(car_url)
-		print(len(car_urls))
+			print(len(car_urls))
 	except:
 		print('error')
 
-columns = ['version' , 'year' , 'engine' , 'current_mileage' , 'hand' , 'gearBox' , 'color' , 'original_onership' , 'next_test_in' , 'annual_licensing_fee' , 'price']
-df_car_urls=pd.Dataframe(np.car_urls, columns
-
+df_car_urls=pd.DataFrame(data = np.array(car_urls))
+df_car_urls.to_csv('cars_urls_data.csv')
 
 
 
