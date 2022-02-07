6314a620f60a1b7a803451b51c569542227a02c2
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2022-02-07 12:38:54 +0200

    "starting to achive the details for one car"

--- start _project.py
+++ start _project.py
@@ -1,17 +1,48 @@
 from turtle import pd
-import requests
+import pandas as pd
+import time
+import numpy as np
+import pandas as pd
+import requests 
 from bs4 import BeautifulSoup
-#import pandas as pd
+v_indexes = [ 0 ]
+eleven_columns = ['version' , 'year' , 'engine' , 'current_mileage' , 'hand' , 'gearBox' , 'color' , 'original_onership' , 'next_test_in' , 'annual_licensing_fee' , 'price']
+nine_columns = ['version' , 'year' , 'engine' , 'current_mileage' , 'hand' , 'gearBox' , 'color' , 'original_onership' , 'annual_licensing_fee' ]
+Details = pd.DataFrame(index= v_indexes , columns= v_columns )
+cars = pd.read_csv(r"C:\Users\avoav\Introduction to Data Science\Project סמסטר א\project-semester-a\DataFrame_cars_urls\cars_urls_data.csv" ,index_col=0 )
 
+cars = cars.rename( columns = {'0' : 'url'} )
 
-one_car = requests.get('https://carwiz.co.il/used-cars/1b703714-c6c6-4c40-a62a-a445532dab9f/2018-%D7%A7%D7%99%D7%94-%D7%A1%D7%A4%D7%95%D7%A8%D7%98%D7%96')
+# for i in cars.index:
+one_car = requests.get(cars['url'][0]) 
 soup = BeautifulSoup(one_car.content , 'html.parser')
-# Details = soup.find('div' , class_='MuiBox-root jss1013 jss1007')
-# print (Details)
+    
+    # how to choose only the second td each time?
+    
+Det = soup.select('td' , class_='MuiTableCell-root MuiTableCell-body MuiTableCell-alignLeft MuiTableCell-paddingNone')
+df_Det = pd.DataFrame( columns= v_columns)
+det_array = []
+for i in range(len(Det)):
+    if (i%2 != 0):
+        det_array.append(Det[i].get_text())
+        if (Det[i].get_text() == None):
+            det_array.append(None)
+if len(det_array) ==9:
+    df_det_array = pd.DataFrame(np.array([det_array]) , columns = nine_columns  ) 
+df_det_array 
+#how to append data with different number of columns 
+df_Det.append(df_det_array , ignore_index = True , sort = True)
+df_Det
+#Details.append(df_Det , ignore_index = True , )
 
-muiBox = soup.select('h1')
-muiBox = soup.select('h1.MuiTypography-root')
+#cars_details = details[0]
+
+#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
 #print (soup.prettify)
+# muiBox = soup.select('h1')
+# muiBox = soup.select('h1.MuiTypography-root')
+# details = soup.select('h2.MuiTypography-root[tabindex="0"]')
 
 
 # car_type = soup.select()
